7c22fc19bfb8ad1a4d929749b0aa86860894f28c
mframosg <53051495+mframosg@users.noreply.github.com>
2020-08-13 16:46:13 -0500

    Add files via upload

---
+++ Php project/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator - Miguel Ramos</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <p>Nombre: <input type="text" name="nombre" id=""><br><br></p>
+        <p>Nota1: <input type="text" name="nombre" id=""><br><br></p>
+        <p>Nota2: <input type="text" name="nombre" id=""><br><br></p>
+        <p>Nota3: <input type="text" name="nombre" id=""><br><br></p>
+        <p>Nota4: <input type="text" name="nombre" id=""><br><br></p>
+        <p><input type="submit" value="Calculate"></p>
+
+
+    </form>
+
+
+
+    <?php
+
+    
+    ?>
+
+</body>
+
+</html>
